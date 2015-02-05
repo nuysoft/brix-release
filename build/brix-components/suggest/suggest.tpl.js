591a2b946965557ae1d9b6d34df05286fafb3e00
墨智 <mozhi.gyy@alibaba-inc.com>
2015-02-05 14:28:49 +0800

    移除非覆盖发布文件

--- build/brix-components/suggest/suggest.tpl.js
+++
@@ -1,8 +0,0 @@
-/* global define */
-define(function() {
-    return (function(){/*
-<div class="suggest">
-    <ul class="dropdown-menu"></ul>
-</div>
-    */}).toString().split('\n').slice(1,-1).join('\n') + '\n'
-})
