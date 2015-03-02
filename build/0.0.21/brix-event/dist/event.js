d4e3ce4c04bab0293806b1f719e0ad92b47ba31b
墨智 <mozhi.gyy@alibaba-inc.com>
2015-03-02 12:32:08 +0800

    bower update

--- build/0.0.21/brix-event/dist/event.js
+++ build/0.0.21/brix-event/dist/event.js
@@ -9,7 +9,7 @@ define(
         jQuery, _
     ) {
 
-        var DEBUG = ~location.search.indexOf('debug') && {
+        var DEBUG = ~location.search.indexOf('brix.event.debug') && {
             fix: function(arg, len) {
                 var fix = parseInt(len, 10) - ('' + arg).length
                 for (var i = 0; i < fix; i++) arg += ' '
