acde9cd7f71d8ed0af1193ce8024f89f7b7e0dd4
墨智 <mozhi.gyy@alibaba-inc.com>
2015-03-09 16:58:28 +0800

    bower update

--- build/0.0.21/brix-components/pagination/pagination.js
+++ build/0.0.21/brix-components/pagination/pagination.js
@@ -86,6 +86,16 @@ define(
                 this.trigger('change.pagination', this._state)
                 this.render()
             },
+            total: function(total) {
+                if (total === undefined || total === null) return this._state.total
+                this._state.setTotal(total)
+                return this
+            },
+            cursor: function(cursor) {
+                if (cursor === undefined || cursor === null) return this._state.cursor
+                this._state.setCursor(cursor)
+                return this
+            },
             fixData: function() {
                 var barStart = Math.min(
                     this._state.pages,
