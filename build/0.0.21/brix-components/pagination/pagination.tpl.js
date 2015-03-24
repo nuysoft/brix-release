2ae2bcdc28ca6b21dba7b9e32f47695af57c367e
墨智 <mozhi.gyy@alibaba-inc.com>
2015-03-24 16:05:32 +0800

    bower update

--- build/0.0.21/brix-components/pagination/pagination.tpl.js
+++ build/0.0.21/brix-components/pagination/pagination.tpl.js
@@ -1,7 +1,7 @@
 /* global define */
 define(function() {
     return (function(){/*
-<div class="row">
+<div class="row paginationwrapper">
     <div class="pagination-statistics col-md-6">
         当前第 <b><%= start + 1 %> - <%=end%></b> 条，共 <b><%= total %></b> 条，每页展现
         <select bx-name="components/dropdown">
