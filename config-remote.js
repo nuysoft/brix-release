fb6be6731844124229ab492d476ae5f2fa2444ee
墨智 <mozhi.gyy@alibaba-inc.com>
2015-06-01 17:30:46 +0800

    + config-remote.js: bust arg

--- config-remote.js
+++ config-remote.js
@@ -18,6 +18,7 @@
     // http://requirejs.org/docs/api.html
     require.config({
         waitSeconds: 15, // http://requirejs.org/docs/api.html#config-waitSeconds
+        // urlArgs: "bust=" + (new Date()).getTime() // http://requirejs.org/docs/api.html#config-urlArgs
         map: { // http://requirejs.org/docs/api.html#config-map
             '*': {
                 // RequireJS 插件
