fdeef285f9cff6def66d08dc88522fba029b8ce4
墨智 <mozhi.gyy@alibaba-inc.com>
2015-10-27 15:47:10 +0800

    bower update

---
+++ build/0.0.25/css.js
@@ -0,0 +1,6 @@
+/* global require, document, location */
+(function() {
+	require(['css!dependencies/bootstrap/dist/css/bootstrap'])
+	// require(['css!css-tool/mm.css'])
+	require(['css!css-tool/minecraft.css'])
+})();
