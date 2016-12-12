8fc186b09365ad160feb2480f7720afe39b0b6d2
墨智 <mozhi.gyy@alibaba-inc.com>
2016-12-12 15:16:41 +0800

    bower update

--- build/require-config-css-animation.js
+++ build/require-config-css-animation.js
@@ -2144,7 +2144,7 @@ var requirejs, require, define;
 
         if (daily || publish) {
             local = false
-            if (/brix-release\/$/.test(base)) base += '1.0.0-alpha.7/'
+            if (/brix-release\/$/.test(base)) base += '1.0.0-alpha.8/'
         }
         if (local || daily) debug = true
 
