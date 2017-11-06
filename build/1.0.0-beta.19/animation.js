00d541a76bcd092f86933975648895295b72e83a
zuomo <zuomo.xb@taobao.com>
2017-11-06 15:10:59 +0800

    bower update

--- build/1.0.0-beta.19/animation.js
+++ build/1.0.0-beta.19/animation.js
@@ -1,5 +1,1 @@
-/* global require */
-(function() {
-	// 动画 Animation
-	require(['css!brix/deps/minecraft-animation/dist/css/animate-min.css'])
-})();
+!function(){require(["css!brix/deps/minecraft-animation/dist/css/animate-min.css"])}();
--- build/1.0.0-beta.19/css.js
+++ build/1.0.0-beta.19/css.js
@@ -1,12 +1,1 @@
-/* global require */
-(function() {
-	// 基于 Bootstrap
-	require(['css!brix/deps/bootstrap/dist/css/bootstrap.min.css'])
-	
-	// 所有组件样式
-	require(['css!brix/styles/components.css'])
-	require(['css!brix/deps/parsleyjs/src/parsley.css'])
-
-	// MineCraft 规范样式
-	require(['css!brix/styles/minecraft.css'])
-})();
+!function(){require(["css!brix/deps/bootstrap/dist/css/bootstrap.min.css"]),require(["css!brix/styles/components.css"]),require(["css!brix/deps/parsleyjs/src/parsley.css"]),require(["css!brix/styles/minecraft.css"])}();
